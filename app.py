3c80397d15fd012406b8b99d2018a909a1ca39b0
michelsmz <michelsouza.mz@gmail.com>
2026-03-04 09:01:41 -0300

    initialize Streamlit app and add requirements file

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Pedrinhooooo Bonitinhoooo!")
+st.write("Pedrinhooooo Bonitinhoooo!")
+
+# Testando variáveis
+nome_do_projeto = "Missão Marte"
+st.write("O nome do meu projeto é:", nome_do_projeto)
